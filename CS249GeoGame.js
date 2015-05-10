09ca383a9411bff9056faf248eade94c6c334c64
priscilla-lee <plee3@wellesley.edu>
2015-05-09 21:51:39 -0400

    about to try merging markers and places into a single mongodb collection

--- CS249GeoGame.js
+++ CS249GeoGame.js
@@ -1,5 +1,7 @@
 Markers = new Mongo.Collection('markers');
 Places = new Mongo.Collection('places');
+
+var markers = {};
 var index = 0;
 
 var placeNames = ["Sydney Opera House", "Stanford University", "Statue of Liberty New York", "Taj Mahal", 
@@ -13,6 +15,7 @@ if (Meteor.isClient) {
 	
 	Session.set('currentPage', 'home');
 	Session.set('currentPlace', placeNames[index++]); //'Sydney Opera House');
+	Session.set('currentView', 'static');
 	
 	Template.body.helpers({
         displayHome: function() {return Session.get('currentPage')=='home';},
@@ -29,20 +32,30 @@ if (Meteor.isClient) {
 		"click #viewInstructions": function() {Session.set('currentPage', 'instructions');}
     });
 	
-	Template.game.events({
-		"click .goHome": function() {Session.set('currentPage', 'home');}
+	Template.gameButtons.events({
+		"click .goHome": function() {
+			Session.set('currentPage', 'home');
+		},
+		"click .streetView": function() {
+			if (Session.get("currentView")=="static") {
+				Session.set("currentView", "street");
+			} else {
+				Session.set("currentView", "static");
+			}
+		}
     });
 	
-	Template.stats.events({
-		"click .goHome": function() {Session.set('currentPage', 'home');}
-    });
+	Template.list.helpers({
+		places: function() {
+			return Places.find({recommended: {$gt: 3}}).fetch();
+		}
+	});
 	
 	Template.list.events({
-		"click .goHome": function() {Session.set('currentPage', 'home');}
-    });
-	
-	Template.instructions.events({
-		"click .goHome": function() {Session.set('currentPage', 'home');}
+		"click .recPlace": function() {
+			var placeName = document.getElementById("placeRec").value;
+			Meteor.call("recommendPlace", placeName);
+		}
     });
 	
 	Template.game.helpers({
@@ -56,6 +69,14 @@ if (Meteor.isClient) {
 			var name = Session.get('currentPlace');
 			var place = Places.findOne({name:name});
 			return place.streetView;
+		},
+		displaySM: function() {
+			if (Session.get("currentView")=="static") {return "block";}
+			else {return "none";}
+		},
+		displaySV: function() {
+			if (Session.get("currentView")=="street") {return "block";}
+			else {return "none";}	
 		}
 	});
 	
@@ -66,46 +87,79 @@ if (Meteor.isClient) {
 	Template.next.events({
 		"click #nextlevel": function() {
 			$('#nextModal').modal('hide');
-			Session.set("currentPlace", placeNames[index++]);
+			
+			//add to solved
+			var place = Places.findOne({name:Session.get('currentPlace')});
+			Places.update(place._id, {$push: {solvedBy: Meteor.userId()}});
+
+			//change color of marker
+			var m = Markers.findOne({place: Session.get("currentPlace")});
+			var marker = markers[m._id];
+			marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+				
+			//next place
+			Session.set("currentPlace", placeNames[index++]);	
+			
+			//set new street view
+			var sv = GoogleMaps.maps.streetView.instance;
+			sv.setPosition({lat: place.geocode.latitude, lng: place.geocode.longitude});
+			google.maps.event.trigger(sv, 'resize'); 
+			
+			//reset zoom level 
+			var map = GoogleMaps.maps.map.instance;
+			map.setZoom(1);
 		}
 	});
 	
 
 	Template.map.onCreated(function() {
 		GoogleMaps.ready('map', function(map) {
-			console.log("I'm ready!");
-		  google.maps.event.addListener(map.instance, 'click', function(event) {
-			Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-			$('#nextModal').modal('show');
-		  });
+			google.maps.event.addListener(map.instance, 'center_changed', function(event) {
+				var answer = Places.findOne({name:Session.get('currentPlace')}).geocode;
+				var player = map.instance.center;
+				
+				Meteor.call("checkCenter", answer, player, function(error, result) {
+					var distance = result;	
+					var zoom = GoogleMaps.maps.map.instance.getZoom();
+					if (distance < 0.0005 && zoom > 16) {$('#nextModal').modal('show');}
+					else {console.log("still " + distance + " away");}
+				});
+			});
+			
+			Markers.find().observe({
+				added: function (document) {
+					
+					// var marker = new google.maps.Circle({
+						// strokeColor: "#FF0000",
+						// strokeOpacity: 0.8,
+						// strokeWeight: 2,
+						// fillColor: '#FF0000',
+						// fillOpacity: 0.35,
+						// map: map.instance,
+						// center: new google.maps.LatLng(document.lat, document.lng),
+						// radius: 100,
+						// id: document._id
+					// });
+					
+				  var marker = new google.maps.Marker({
+					animation: google.maps.Animation.DROP,
+					position: new google.maps.LatLng(document.lat, document.lng),
+					map: map.instance,
+					id: document._id,
+					icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+				  });
 
-		  var markers = {};
-
-		  Markers.find().observe({
-			added: function (document) {
-			  var marker = new google.maps.Marker({
-				draggable: true,
-				animation: google.maps.Animation.DROP,
-				position: new google.maps.LatLng(document.lat, document.lng),
-				map: map.instance,
-				id: document._id
-			  });
-
-			  google.maps.event.addListener(marker, 'dragend', function(event) {
-				Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-			  });
-
-			  markers[document._id] = marker;
-			},
-			changed: function (newDocument, oldDocument) {
-			  markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-			},
-			removed: function (oldDocument) {
-			  markers[oldDocument._id].setMap(null);
-			  google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-			  delete markers[oldDocument._id];
-			}
-		  });
+				  markers[document._id] = marker;
+				},
+				changed: function (newDocument, oldDocument) {
+				  markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+				},
+				removed: function (oldDocument) {
+				  markers[oldDocument._id].setMap(null);
+				  google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+				  delete markers[oldDocument._id];
+				}
+			});
 		});
 	  });
 
@@ -147,30 +201,62 @@ if (Meteor.isServer) {
 			var result = geo.geocode(name)[0];
 			
 			var staticMap = baseURL + result.latitude + "," + result.longitude;
-			
-			console.log(staticMap);
-			
-			// var streetViewDiv = document.createElement("div");
-			// streetViewDiv.className = "street-view";
-			// var streetViewOptions = {position: {lat: result.latitude, lng: result.longitude}};
-			// var streetView = new google.maps.StreetViewPanorama(streetViewDiv, streetViewOptions);
-				
+
 			Places.insert({
 				name: name, 
 				geocode: result, 
-				staticMap: staticMap
-				// streetView: streetViewDiv
+				staticMap: staticMap,
+				recommended: 5,
+				solvedBy: []
 			});
-		}	
+			
+			Markers.insert({ 
+				lat: result.latitude, 
+				lng: result.longitude,
+				place: name
+			});
+		}
 	}
+	
+	Meteor.methods({
+		'recommendPlace': function(name) {
+			var result = geo.geocode(name)[0];
+			if (result==undefined) {
+				alert("That's not a real place, is it?");
+			} else {
+				var staticMap = baseURL + result.latitude + "," + result.longitude;
+				
+				Places.insert({
+					name: name, 
+					geocode: result, 
+					staticMap: staticMap,
+					recommended: 1,
+					solvedBy: []
+				});
+			}
+		},
+		'checkCenter': function(answer, player) {
+			var alat = answer.latitude;
+			var alng = answer.longitude;
+			var plat = player.A;
+			var plng = player.F;
 
-	/*
-	// Reverse
-	var geo = new GeoCoder({
-	  geocoderProvider: "mapquest",
-	  httpAdapter: "https",
-	  apiKey: 'YOUR_API_KEY'
+			//make sure these numbers are not negative
+			while (alat < 0) alat += 360;
+			while (alng < 0) alng += 360;
+			while (plat < 0) plat += 360;
+			while (plng < 0) plng += 360;
+			
+			//make sure these numbers are below 360
+			alat = alat%360; alng = alng%360; plat = plat%360; plng = plng%360;
+
+			//these coordinates should all now be between 0 and 360
+			var distance = {
+				lat: Math.abs(alat - plat),
+				lng: Math.abs(alng - plng)
+			}; 
+			
+			return (distance.lat + distance.lng);
+		}
 	});
-	var result = geo.reverse(45.767, 4.833);
-	*/
 }
